9c9dfc31698fa838e6acf4122296636866e1888f
Artem <webgid.main@gmail.com>
2025-06-24 15:13:29 +0300

    init project

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const SheetHandler = require('./helpers/spreadsheet');
 const mysqlHandler = require('./helpers/bd');
 const ttHandler = require('./helpers/tiktok');
 const path = require('path');
+console.log(process.env.SPREADSHEET)
+console.log(process.env.HOST)
 const pathWay = process.cwd().indexOf("domains") == -1 ? "/root/ttstats" : "D:\\OpenServer\\domains\\ttstats"
 //require('dotenv').config({path: path.join(pathWay, `.env`)});
 require('dotenv').config({});
